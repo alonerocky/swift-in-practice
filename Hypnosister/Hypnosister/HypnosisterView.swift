85c9442355ee5c6a57cfe431de9e8fa7aa73d679
shoulong li <lishoulong@gmail.com>
2015-05-11 23:54:39 -0700

    Hypnosister project checkin

---
+++ Hypnosister/Hypnosister/HypnosisterView.swift
@@ -0,0 +1,21 @@
+//
+//  HypnosisterView.swift
+//  Hypnosister
+//
+//  Created by shoulong li on 5/11/15.
+//  Copyright (c) 2015 shoulong li. All rights reserved.
+//
+
+import UIKit
+
+class HypnosisterView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
