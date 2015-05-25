096adbd6bf9e98b6b45b56b01a62384db8051662
shoulong li <lishoulong@gmail.com>
2015-05-24 21:47:31 -0700

    Homepwner initial commit

---
+++ Homepwner/Homepwner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Homepwner
+//
+//  Created by shoulong li on 5/24/15.
+//  Copyright (c) 2015 shoulong li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
