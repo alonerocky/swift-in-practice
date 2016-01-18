ca2ba6f26055c89663540336f00dca96cbd51668
alonerocky <lishoulong@gmail.com>
2016-01-18 09:17:04 -0800

    Two sum - refactoring

--- TwoSumI.swift
+++ TwoSumI.swift
@@ -16,13 +16,12 @@ func twoSum(nums: [Int], _ target: Int) -> [Int] {
         let delta = target - key
         //found one
         if let indices = dict[delta] {
-            if delta == key && indices.count >= 2{
-                result = [indices[0] + 1, indices[1] + 1]
-            } else if delta != key {
+            if delta != key {
                 result = dict[key]![0] < dict[delta]![0] ? [dict[key]![0] + 1, dict[delta]![0] + 1] : [dict[delta]![0] + 1, dict[key]![0] + 1]
+            } else if indices.count >= 2 {
+                result = [indices[0] + 1, indices[1] + 1]
             }
         }
     }
     return result
 }
-
