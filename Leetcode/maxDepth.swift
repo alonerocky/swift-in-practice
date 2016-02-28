c6b4f6e32d7d78848ad0b88e4a9a9e4c69ba8188
alonerocky <lishoulong@gmail.com>
2016-02-27 22:06:45 -0800

    104. Maximum Depth of Binary Tree

---
+++ Leetcode/maxDepth.swift
@@ -0,0 +1,7 @@
+func maxDepth(root: TreeNode?) -> Int {
+        if root == nil {
+            return 0
+        } else {
+            return 1 + max(maxDepth(root!.left), maxDepth(root!.right))
+        }
+    }
