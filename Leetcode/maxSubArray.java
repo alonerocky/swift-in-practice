9cc2a197165870c4c06a6919b268d3311dda35c7
alonerocky <lishoulong@gmail.com>
2016-03-03 09:41:09 -0800

    53. Maximum Subarray

---
+++ Leetcode/maxSubArray.java
@@ -0,0 +1,15 @@
+public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int current = nums[0];
+        int maxSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (current < 0) {
+                current = 0;
+            }
+            current += nums[i];
+            maxSum = Math.max(maxSum, current);
+        }
+        return maxSum;
+    }
