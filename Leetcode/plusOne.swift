57e8983a5445731dbb5199351c3b8cc83ed8b359
alonerocky <lishoulong@gmail.com>
2016-03-04 19:48:37 -0800

    fix one bug

--- Leetcode/plusOne.swift
+++ Leetcode/plusOne.swift
@@ -5,7 +5,7 @@ func plusOne(digits: [Int]) -> [Int] {
         var result: [Int] = [Int](count: digits.count, repeatedValue: 0)
         var carray: Int = 0
         for var i = digits.count - 1 ; i >= 0; i-- {
-            let temp = digits[i] + (i == 0 ? 1 : 0) + carray
+            let temp = digits[i] + (i == digits.count - 1 ? 1 : 0) + carray
             result[i] = temp % 10
             carray = temp / 10
         }
