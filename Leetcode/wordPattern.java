eccf92fbcbf6b37c3947321804142a66d90a745b
alonerocky <lishoulong@gmail.com>
2016-03-11 10:22:40 -0800

    wordPattern change

--- Leetcode/wordPattern.java
+++ Leetcode/wordPattern.java
@@ -1,23 +1,22 @@
 public static boolean wordPattern(String pattern, String str) {
         HashMap<Character, String> map1 = new HashMap<Character, String>();
         HashMap<String, Character> map2 = new HashMap<String, Character>();
+
         String[] words = str.split(" ");
         if (words.length != pattern.length()) {
             return false;
         }
-        
-        for (int i =0; i < pattern.length(); i++) {
+        int len = pattern.length();
+        for (int i =0 ;i < len; i++) {
             char ch = pattern.charAt(i);
             String word = words[i];
-            if (!map1.containsKey(ch) && !map2.containsKey(word)) {
+            if (!map1.containsKey(ch)) {
                 map1.put(ch, word);
+            }
+            if (!map2.containsKey(word)) {
                 map2.put(word, ch);
-            } else if (map1.containsKey(ch) && 
-                    map2.containsKey(word) && 
-                    map1.get(ch).equals(word) &&
-                    map2.get(word) == ch){
-                continue;
-            } else {
+            }
+            if (!map1.get(ch).equals(word) || map2.get(word) != ch) {
                 return false;
             }
         }
