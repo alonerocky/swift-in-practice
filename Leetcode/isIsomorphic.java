49415eca36a5069084fa2d1053e539702a3592e1
alonerocky <lishoulong@gmail.com>
2016-03-11 10:21:54 -0800

    isIsomorphic change

--- Leetcode/isIsomorphic.java
+++ Leetcode/isIsomorphic.java
@@ -1,23 +1,25 @@
 public boolean isIsomorphic(String s, String t) {
         HashMap<Character, Character> map1 = new HashMap<Character, Character>();
         HashMap<Character, Character> map2 = new HashMap<Character, Character>();
-        int len = s.length();
-        int i =0; 
-        while (i < len) {
-            char left = s.charAt(i);
-            char right = t.charAt(i);
-            if (!map1.containsKey(left) && !map2.containsKey(right)) {
-                map1.put(left, right);
-                map2.put(right, left);
-                i++;
-            } else if (map1.containsKey(left) && map2.containsKey(right)) {
-                if(map1.get(left) != right || map2.get(right) != left) {
+        if (s.equals(t)) {
+            return true;
+        }
+        else if (s.length() != t.length()) {
+            return false;
+        } else {
+            for (int i =0 ;i < s.length(); i++) {
+                char ch1 = s.charAt(i);
+                char ch2 = t.charAt(i);
+                if (!map1.containsKey(ch1)) {
+                    map1.put(ch1, ch2);
+                }
+                if (!map2.containsKey(ch2)) {
+                    map2.put(ch2, ch1);
+                }
+                if (map1.get(ch1) != ch2 || map2.get(ch2) != ch1) {
                     return false;
                 }
-                i++;
-            } else {
-                return false;
             }
+            return true;
         }
-        return true;
     }
