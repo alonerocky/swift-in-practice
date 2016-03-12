623f41b162a4b0566ded2450d8cef9a271da92ac
alonerocky <lishoulong@gmail.com>
2016-03-12 10:45:56 -0800

    292. Nim Game

---
+++ Leetcode/canWinNim.swift
@@ -0,0 +1,10 @@
+/*
+    [1,2,3]  Nim win
+    [4]  Nim lose
+    [5,6,7] , Nim can take 1-3, become it to [4], Nim win
+    [8], no matter Nim take home many, it will become [5,6,7]. so Nim lose
+    */
+    
+    func canWinNim(n: Int) -> Bool {
+        return n % 4 != 0
+    }
