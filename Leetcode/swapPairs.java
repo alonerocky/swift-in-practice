69f8af34bb68f248471b1f042e5dfb4c067caa20
alonerocky <lishoulong@gmail.com>
2016-03-12 22:04:11 -0800

    24. Swap Nodes in Pairs

---
+++ Leetcode/swapPairs.java
@@ -0,0 +1,27 @@
+public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode p = head;
+        ListNode result = null;
+        ListNode tail = null;
+        while (p != null && p.next != null) {
+            ListNode one = p;
+            ListNode two = p.next;
+            p = p.next.next;
+            //swap
+            one.next = null;
+            two.next = one;
+            if (result == null) {
+                result = two;
+                tail = one;
+            } else {
+                tail.next = two;
+                tail = one;
+            }
+        }
+        if (p != null) { //nodes number is odd
+            tail.next = p;
+        }
+        return result;
+    }
