4ad39f942bf76ce8b493689fbce4d568b76c710a
alonerocky <lishoulong@gmail.com>
2016-03-16 08:49:34 -0700

    136. Single Number

---
+++ Leetcode/singleNumber.java
@@ -0,0 +1,7 @@
+public int singleNumber(int[] nums) {
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
