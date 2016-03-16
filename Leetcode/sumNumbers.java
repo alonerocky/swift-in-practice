a20ef2ac399a128d3339014017d7068ca748759f
alonerocky <lishoulong@gmail.com>
2016-03-15 18:26:30 -0700

    129. Sum Root to Leaf Numbers

--- Leetcode/sumNumbers.java
+++ Leetcode/sumNumbers.java
@@ -4,20 +4,10 @@ public int sumNumbers(TreeNode root) {
     public int sumNumbers(TreeNode root, int base) {
         if(root == null)
             return base;
-        
-        if(root.left != null && root.right != null)
-        {
-            return sumNumbers(root.left , 10 * base + root.val) + sumNumbers(root.right,10 * base + root.val);
-        }
-        else if(root.left != null)
-        {
-            return sumNumbers(root.left , 10 * base + root.val) ;
-        }
-        else if(root.right != null)
-        {
-            return sumNumbers(root.right,10 * base + root.val);
-        }
-        else
+        if (root.left == null && root.right == null) {
             return 10 * base + root.val;
-            
+        }
+        int left = root.left == null ? 0 : sumNumbers(root.left , 10 * base + root.val);
+        int right = root.right == null ? 0: sumNumbers(root.right,10 * base + root.val);
+        return left + right;
     }
