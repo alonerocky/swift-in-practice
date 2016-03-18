ec01f1336388cea43803ef40c7d7cab57100b96a
alonerocky <lishoulong@gmail.com>
2016-03-18 13:39:28 -0700

    263. Ugly Number

---
+++ Leetcode/isUgly.java
@@ -0,0 +1,34 @@
+public boolean isUglyI(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        if (num == 1) {
+            return true;
+        }
+        if (num % 2== 0 && isUgly(num /2) || num % 3 == 0 && isUgly(num / 3) || num % 5 == 0 && isUgly(num / 5)) {
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean isUgly(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        if (num == 1) {
+            return true;
+        }
+        while (num > 1) {
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else if (num % 3 == 0) {
+                num = num / 3;
+            } else if (num % 5 == 0) {
+                num = num / 5;
+            } else {
+                return false;
+            }
+        }
+        return true;
+        
+    }
