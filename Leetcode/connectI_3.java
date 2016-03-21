629133f63d0c865bbbfb333620d55162c33a14dd
alonerocky <lishoulong@gmail.com>
2016-03-20 22:30:54 -0700

    116	Populating Next Right Pointers in Each Node

---
+++ Leetcode/connectI_3.java
@@ -0,0 +1,11 @@
+public void connect(TreeLinkNode root) {
+        if (root == null) {
+            return;
+        }
+        if (root.left != null) {
+            root.left.next = root.right;
+            root.right.next = root.next == null ? null : root.next.left;
+        }
+        connect(root.left);
+        connect(root.right);
+    }
