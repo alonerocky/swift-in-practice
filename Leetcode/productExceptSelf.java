a8fd0138051a70963cb7c175dbb3bc4a27710cce
alonerocky <lishoulong@gmail.com>
2016-03-29 22:55:32 -0700

    238. Product of Array Except Self

---
+++ Leetcode/productExceptSelf.java
@@ -0,0 +1,21 @@
+public int[] productExceptSelf(int[] nums) {
+        if (nums == null) {
+            return null;
+        } else if (nums.length == 0) {
+            return nums;
+        } 
+        int len = nums.length;
+        int[] left = new int[len];
+        int[] right = new int[len];
+        left[0] = 1;
+        right[len-1] = 1;
+        for (int i = 1; i < len; i++) {
+            left[i] = left[i-1] * nums[i-1];
+            right[len-1-i] = right[len-i] * nums[len-i];
+        }
+        int[] result = new int[len];
+        for (int i = 0; i < len; i++) {
+            result[i] = left[i] * right[i];
+        }
+        return result;
+    }
