66f486490f9c17d2050e4930084590f84f0ad8f1
alonerocky <lishoulong@gmail.com>
2016-04-02 22:53:07 -0700

    172. Factorial Trailing Zeroes

---
+++ Leetcode/trailingZeros.java
@@ -0,0 +1,12 @@
+//http://www.geeksforgeeks.org/count-trailing-zeroes-factorial-number/
+    /*
+    Trailing 0s in n! = Count of 5s in prime factors of n!
+                  = floor(n/5) + floor(n/25) + floor(n/125) + ....
+    */
+    public int trailingZeroes(int n) {
+        int count = 0;
+        for (int i = 5; n/i >= 1; i *= 5) {
+            count += n/i
+        }
+        return count;
+    }
