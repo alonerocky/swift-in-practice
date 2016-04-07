07f9fe2798fdecbddfd574912ffe5a8806907241
alonerocky <lishoulong@gmail.com>
2016-04-06 18:54:57 -0700

    274	H-Index

--- Leetcode/hIndex.java
+++ Leetcode/hIndex.java
@@ -14,3 +14,38 @@ public int hIndex(int[] citations) {
         }
         return 0;
     }
+
+
+public int hIndex(int[] citations) {
+        if (citations == null || citations.length == 0) {
+            return 0;
+        }
+        
+        Arrays.sort(citations);
+        reverse(citations);
+        
+        //decsending order
+        int i = 0;
+        for (;i < citations.length; i++) {
+            if (citations[i] <= i) { //citations[i] >= i + 1
+                break;
+            }
+        }
+        
+        return i;
+    }
+    
+    private void reverse(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return;
+        }
+        int i = 0; 
+        int j = nums.length - 1;
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
