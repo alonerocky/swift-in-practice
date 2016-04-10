8d04f3f500b65ed206151f2f0ac9af824805bc11
alonerocky <lishoulong@gmail.com>
2016-04-10 09:50:44 -0700

    279. Perfect Squares

---
+++ Leetcode/numSquares.java
@@ -0,0 +1,12 @@
+public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j *j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i-j*j] + 1);  //i = a + b* b
+            }
+        }
+        return dp[n];
+    }
