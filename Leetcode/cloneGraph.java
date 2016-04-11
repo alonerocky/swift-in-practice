fa2f3cce1744c64cfe64caf1c5d91730eb63ac59
alonerocky <lishoulong@gmail.com>
2016-04-10 22:32:48 -0700

    133. Clone Graph

--- Leetcode/cloneGraph.java
+++ Leetcode/cloneGraph.java
@@ -1,20 +1,47 @@
 public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+         HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
+         if (node == null) {
+             return null;
+         }
+         return bfs(node);
+    }
+    
+    public UndirectedGraphNode bfs(UndirectedGraphNode node) {
         if (node == null) {
             return null;
         }
+        Deque<UndirectedGraphNode> queue = new ArrayDeque<UndirectedGraphNode>();
         HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
-        return cloneGraph(node, map);
+        UndirectedGraphNode result = new UndirectedGraphNode(node.label);
+        map.put(node, result);
+        queue.addLast(node);
+        //node in queue is already cloned but it's neighbors are not
+        while(!queue.isEmpty()) {
+            UndirectedGraphNode one = queue.pollFirst();
+            UndirectedGraphNode cloned = map.get(one);
+            for (int i = 0; i < one.neighbors.size();i++) {
+                UndirectedGraphNode neighbor = one.neighbors.get(i);
+                if (map.containsKey(neighbor)) {
+                    cloned.neighbors.add(map.get(neighbor));
+                } else {
+                    UndirectedGraphNode c = new UndirectedGraphNode(neighbor.label);
+                    cloned.neighbors.add(c);
+                    map.put(neighbor, c);
+                    queue.addLast(neighbor);
+                }
+            }
+        }
+        return result;
     }
     
-    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node, HashMap<UndirectedGraphNode, UndirectedGraphNode> map) {
+    public UndirectedGraphNode dfs(UndirectedGraphNode node, HashMap<UndirectedGraphNode, UndirectedGraphNode> map) {
         if (map.containsKey(node)) {
             return map.get(node);
         }
         UndirectedGraphNode cloned = new UndirectedGraphNode(node.label);
         map.put(node, cloned);
-        ArrayList<UndirectedGraphNode> neighbors = (ArrayList<UndirectedGraphNode>)node.neighbors;
-        for (int i = 0;i < neighbors.size(); i++) {
-            cloned.neighbors.add(cloneGraph(neighbors.get(i), map));
+        for (int i = 0; i < node.neighbors.size(); i++) {
+            cloned.neighbors.add(dfs(node.neighbors.get(i), map));
         }
         return cloned;
     }
