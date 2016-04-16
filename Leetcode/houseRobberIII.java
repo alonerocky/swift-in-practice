76920da6aed1e7dabc63436c1df6211ffc6c08ba
alonerocky <lishoulong@gmail.com>
2016-04-16 15:49:11 -0700

    337. House Robber III

---
+++ Leetcode/houseRobberIII.java
@@ -0,0 +1,34 @@
+public int rob(TreeNode root) {
+        Entry e = rob_helper(root);
+        return Math.max(e.maxInclude, e.maxExclude);
+    }
+    public Entry rob_helper(TreeNode root) {
+        if (root == null) {
+            return new Entry(0,0);
+        } else if (root.left == null && root.right == null) {
+            return new Entry(0, root.val);
+        } 
+        /* else if (root.left == null) {
+            Entry right = rob_helper(root.right);
+            return new Entry(Math.max(right.maxInclude, right.maxExclude), right.maxExclude + root.val);
+        } else if (root.right == null) {
+            Entry left = rob_helper(root.left);
+            return new Entry(Math.max(left.maxInclude, left.maxExclude), left.maxExclude + root.val);
+        }  */
+        else  {
+            Entry left = rob_helper(root.left);
+            Entry right = rob_helper(root.right);
+            return new Entry(Math.max(left.maxInclude, left.maxExclude) + Math.max(right.maxExclude , right.maxInclude), 
+            root.val + left.maxExclude + right.maxExclude);
+            
+        }
+    }
+    
+    class Entry {
+        public int maxExclude ;
+        public int maxInclude ;
+        public Entry(int maxExclude, int maxInclude) {
+            this.maxExclude = maxExclude;
+            this.maxInclude = maxInclude;
+        }
+    }
