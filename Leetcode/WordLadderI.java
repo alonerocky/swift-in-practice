03c3eb7b6141c53c5253a0bf1db8bc371f4e660b
alonerocky <lishoulong@gmail.com>
2016-04-22 13:57:02 -0700

    day 52

--- Leetcode/WordLadderI.java
+++ Leetcode/WordLadderI.java
@@ -1,4 +1,47 @@
 public int ladderLength(String start, String end, Set<String> dict) {
+        if (dict == null) {
+            return 0;
+        }
+        long before = System.currentTimeMillis();
+        dict.add(start);
+        dict.add(end);
+
+        Deque<WordLadder> queue = new ArrayDeque<WordLadder>();
+        WordLadder first = new WordLadder(start, 1, null);
+        queue.addLast(first);
+        int len = Integer.MAX_VALUE;
+        while(!queue.isEmpty()) {
+            WordLadder front = queue.pollFirst();
+            if (front.len <= len && front.word.equals(end)) {
+                len = front.len;
+                return len;
+            } else if(front.len < len ){
+                ArrayList<String> next = getNextWords(front.word, dict);
+                for(int i = 0; i < next.size();i++) {
+                    String one = next.get(i);
+                    boolean used = false;
+                    WordLadder p = front;
+                    while(p != null) {
+                        if (p.word.equals(one)) {
+                            used = true;
+                            break;
+                        }
+                        p = p.prev;
+                    }
+                    if (!used) {
+                        if (one.equals(end)) {
+                            return front.len + 1;
+                        } else {
+                            WordLadder nextLadder = new WordLadder(one, front.len + 1, front);
+                            queue.add(nextLadder);
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+     public int ladderLengthII(String start, String end, Set<String> dict) {
        if (dict == null) {
             return 0;
         }
@@ -31,6 +74,50 @@ public int ladderLength(String start, String end, Set<String> dict) {
             }
         }
         return 0;
+    }
+    
+    public int ladderLengthIII(String start, String end, Set<String> dict) {
+        if (start.equals(end)) {
+            return 1;
+        }
+        if (dict == null || dict.size() == 0) {
+            return 0;
+        }
+        dict.add(start);
+        dict.add(end);
+        Deque<String> queue = new ArrayDeque<>();
+        queue.add(start);
+        int level = 1;
+        int currentLevel = 1;
+        int nextLevel = 0;
+        HashSet<String> used = new HashSet<>();
+        used.add(start);
+        while(!queue.isEmpty()) {
+            String front = queue.pollFirst();
+
+            currentLevel--;
+
+            ArrayList<String> neighbors = getNextWords(front, dict);
+            for (int i = 0; i < neighbors.size();i++) {
+                String next = neighbors.get(i);
+                if (next.equals(end)) {
+                    return level + 1;
+                }
+                if (!used.contains(next)) {
+                    used.add(next);
+                    queue.add(next);
+                    nextLevel++;
+                }
+            }
+            if (currentLevel == 0) {
+                level++;
+                currentLevel = nextLevel;
+                nextLevel = 0;
+            }
+
+        }
+        return 0;
+
     }
     
     class WordLadder {
