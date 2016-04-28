9c4fe62b67ff1f22d94aeb2d02a496ee0ca3b7a4
alonerocky <lishoulong@gmail.com>
2016-04-28 04:08:35 -0700

    day 57 257

--- Leetcode/longestConsecutive.java
+++ Leetcode/longestConsecutive.java
@@ -20,3 +20,46 @@ public int longestConsecutive(TreeNode root) {
         
         }
     }
+
+
+
+public int longestConsecutive(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Result r = longestConsecutive_helper(root);
+        return r.max;
+       
+    }
+    public Result longestConsecutive_helper(TreeNode root) {
+        if (root == null) {
+            return new Result(0, -1, 0);
+        } else if (root.left == null && root.right == null) {
+            return new Result(1, root.val, 1);
+        } else {
+            Result left = longestConsecutive_helper(root.left);
+            Result right = longestConsecutive_helper(root.right);
+            int len = 1;
+            int max = (left.max == 0 && right.max == 0 ? 1 : Math.max(left.max, right.max));
+            if (left.len > 0 && root.val == left.value - 1) {
+                len = Math.max(left.len + 1, len);
+                max = Math.max(max, len);
+            }
+            if (right.len > 0 && root.val == right.value - 1) {
+                len = Math.max(len, right.len + 1);
+                max = Math.max(max, len);
+            }
+            return new Result(len, root.val, max);
+        }
+    }
+    
+    class Result {
+        int len ;
+        int value;
+        int max;
+        public Result(int len, int value, int max) {
+            this.len = len;
+            this.value = value;
+            this.max = max;
+        }
+    } 
