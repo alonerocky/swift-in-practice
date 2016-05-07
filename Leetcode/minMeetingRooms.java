e7ec0b652e376e1ba504038ed9599ba8b42097ed
alonerocky <lishoulong@gmail.com>
2016-05-07 11:29:57 -0700

    day 67

--- Leetcode/minMeetingRooms.java
+++ Leetcode/minMeetingRooms.java
@@ -1,4 +1,40 @@
 public int minMeetingRooms(Interval[] intervals) {
+          if (intervals == null || intervals.length == 0) {
+              return 0;
+          }
+          int n = intervals.length;
+          int[] start = new int[n];
+          int[] end = new int[n];
+          for (int i =0; i < n; i++) {
+              Interval interval = intervals[i];
+              start[i] = interval.start;
+              end[i] = interval.end;
+          }
+          Arrays.sort(start);
+          Arrays.sort(end);
+          
+          int startP = 0;
+        int endP = 0;
+         
+        int numActive = 0;
+        int numOverlap = 0;
+         
+        while (startP < start.length && endP < end.length) {
+            if (start[startP] < end[endP]) {
+                numActive++;
+                numOverlap = Math.max(numOverlap, numActive);
+                startP++;
+            } else {
+                numActive--;
+                endP++;
+            }
+        }
+        return numOverlap;
+    }
+
+----------------
+
+public int minMeetingRooms(Interval[] intervals) {
          if (intervals == null || intervals.length == 0) {
              return 0;
          }
