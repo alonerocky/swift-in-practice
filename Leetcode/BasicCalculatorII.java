17554f49e1a053875939feced90df5c2542ca3fe
alonerocky <lishoulong@gmail.com>
2016-05-19 12:07:11 -0700

    day 79

--- Leetcode/BasicCalculatorII.java
+++ Leetcode/BasicCalculatorII.java
@@ -77,3 +77,77 @@ public int calculate(String s) {
                 break;
         }
     }
+
+
+//lintcode Expression Evaluation
+public int evaluateExpression(String[] expression) {
+        // write your code here
+        if (expression == null || expression.length == 0) {
+            return 0;
+        }
+        Stack<Integer> numStack = new Stack<Integer>();
+        Stack<Character> opStack = new Stack<Character>();
+        for (int i =0; i < expression.length; i++) {
+            String token = expression[i];
+             
+            if (token.length() == 1 && isOperator(token.charAt(0))) {
+                char op = token.charAt(0);
+                if (op == ')') {
+                    while(!opStack.isEmpty() && opStack.peek() != '(') {
+                        compute(numStack, opStack);
+                    }
+                    opStack.pop(); //pop up ()
+                    
+                } else {
+                    while(!opStack.isEmpty() && needCompute(opStack.peek(), op)) {
+                        compute(numStack, opStack);
+                    }
+                    opStack.push(op);
+                }
+            } else {
+                numStack.push(Integer.parseInt(token));
+            }
+        }
+        while(!opStack.isEmpty()) {
+            compute(numStack, opStack);
+        }
+        if (numStack.isEmpty()) {
+            return 0;
+        }
+        return numStack.pop();
+    }
+    public boolean isOperator(char op) {
+        return op == '+' || op == '-' || op == '*' || op == '/' || op == '(' || op == ')';
+    }
+    public boolean needCompute(char op1, char op2) {
+        switch(op2) {
+            case '(':
+                return false;
+            case '+':
+            case '-':
+                return op1 != '(';
+            case '*':
+            case '/':
+                return op1 == '*' || op1 == '/';
+        }
+        return false;
+    }
+    public void compute(Stack<Integer> numStack, Stack<Character> opStack) {
+        int num2 = numStack.pop();
+        int num1 = numStack.pop();
+        char op = opStack.pop();
+        switch(op) {
+            case '+':
+                numStack.push(num1+num2);
+                break;
+            case '-':
+                numStack.push(num1-num2);
+                break;
+            case '*':
+                numStack.push(num1*num2);
+                break;
+            case '/':
+                numStack.push(num1/num2);
+                break;
+        }
+    }
