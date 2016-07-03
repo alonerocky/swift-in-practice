36832138c48f07360d24dbd158c14e735a514132
alonerocky <lishoulong@gmail.com>
2016-07-03 10:02:48 -0700

    369. Plus One Linked List

--- Leetcode/linkedListPlusOne.java
+++ Leetcode/linkedListPlusOne.java
@@ -3,6 +3,69 @@ public ListNode plusOne(ListNode head) {
             return new ListNode(1);
         }
         ListNode reversed = reverse(head);
+        ListNode p = reversed;
+        ListNode result = null;
+        boolean least = true;
+        int carray = 0;
+        while(p != null) {
+            int r = p.val + carray + (least ? 1 : 0);
+            if (least) {
+                least = false;
+            }
+            ListNode one = p;
+            p = p.next;
+            one.next = null;
+            one.val = r % 10;
+            carray = r / 10;
+            one.next = result;
+            result = one;
+        }
+        if (carray != 0) {
+            ListNode one = new ListNode(1);
+            one.next = result;
+            result = one;
+        }
+        return result;
+        
+         
+    }
+    //create new linked list
+    public ListNode plusOneI(ListNode head) {
+        if (head == null) {
+            return new ListNode(1);
+        }
+        ListNode reversed = reverse(head);
+        ListNode p = reversed;
+        ListNode result = null;
+        boolean least = true;
+        int carray = 0;
+        while(p != null) {
+            int r = p.val + carray + (least ? 1 : 0);
+            if (least) {
+                least = false;
+            }
+            ListNode one = new ListNode(r % 10);
+            carray = r/10;
+            one.next = result;
+            result = one;
+            
+            
+            p = p.next;
+        }
+        if (carray != 0) {
+            ListNode one = new ListNode(1);
+            one.next = result;
+            result = one;
+        }
+        return result;
+        
+         
+    }
+    public ListNode plusOneII(ListNode head) {
+        if (head == null) {
+            return new ListNode(1);
+        }
+        ListNode reversed = reverse(head);
         int carray = 0;
         ListNode p = reversed;
         boolean least = true;
