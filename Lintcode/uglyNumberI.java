c10e9175c3a12f3c556e91f81642b0fbda186927
alonerocky <lishoulong@gmail.com>
2016-07-03 16:56:09 -0700

    Ugly Number

---
+++ Lintcode/uglyNumberI.java
@@ -0,0 +1,21 @@
+public boolean isUgly(int num) {
+        // Write your code here
+        if (num <= 0) {
+            return false;
+        }
+        if (num == 1) {
+            return true;
+        }
+        while(num != 1) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else if (num % 3 == 0) {
+                num /= 3;
+            } else if (num % 5 == 0) {
+                num /= 5;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
