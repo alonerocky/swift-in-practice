0c1f86787198dd7264f0cf903973256fe14290af
alonerocky <lishoulong@gmail.com>
2016-07-03 18:57:55 -0700

    Ugly Number

--- Lintcode/uglyNumberII.java
+++ Lintcode/uglyNumberII.java
@@ -1,3 +1,34 @@
+http://buttercola.blogspot.com/2015/09/leetcode-ugly-number-ii.html
+public int nthUglyNumber(int n) {
+        if (n <= 0) {
+            return -1;
+        }
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        nums.add(1);
+        int i = 0;//2
+        int j = 0;//3
+        int k = 0;//5
+        while(nums.size() < n) {
+            int m2 = nums.get(i) * 2;
+            int m3 = nums.get(j) * 3;
+            int m5 = nums.get(k) * 5;
+            int min = Math.min(m2, Math.min(m3, m5));
+            nums.add(min);
+            
+            if (m2 == min) {
+                i++;
+            }
+            if (m3 == min) {
+                j++;
+            }
+            if (m5 == min) {
+                k++;
+            }
+            
+        }
+        return nums.get(n-1);
+    }
+
 public int nthUglyNumber(int n) {
         // Write your code here
         HashSet<Long> set = new HashSet<Long>();
--- Lintcode/uglyNumberIII.java
+++ Lintcode/uglyNumberIII.java
@@ -1,3 +1,33 @@
+//http://buttercola.blogspot.com/2016/01/leetcode-super-ugly-number.html
+public int nthSuperUglyNumber(int n, int[] primes) {
+        if (n <= 0 || primes == null || primes.length == 0) {
+            return -1;
+        }
+        int k = primes.length;
+        int[] indices = new int[k];
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        nums.add(1);
+        while(nums.size() < n) {
+            int next = Integer.MAX_VALUE;
+            for(int i = 0; i < k; i++) {
+                next = Math.min(next, nums.get(indices[i]) * primes[i]);
+            }
+            nums.add(next);
+            if (nums.size() == n) {
+                break;
+            }
+            
+            //update indices
+            for(int i = 0; i < k; i++) {
+                if (next == nums.get(indices[i]) * primes[i]) {
+                    indices[i]++;
+                }
+            }
+        }
+        return nums.get(n-1);
+        
+    }
+
 public int nthSuperUglyNumber(int n, int[] primes) {
         // Write your code here
         if (n <= 0 || primes == null || primes.length == 0) {
