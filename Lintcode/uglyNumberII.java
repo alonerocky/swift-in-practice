195bacddb13d95b4b7709b755c97138b85ee148c
alonerocky <lishoulong@gmail.com>
2016-07-03 17:14:06 -0700

    Ugly Number II

---
+++ Lintcode/uglyNumberII.java
@@ -0,0 +1,28 @@
+public int nthUglyNumber(int n) {
+        // Write your code here
+        HashSet<Long> set = new HashSet<Long>();
+        set.add(1l);
+        PriorityQueue<Long> minHeap = new PriorityQueue<Long>();
+        minHeap.offer(1l);
+        long result = 1l;
+        for(int i = 0; i < n;i++) {
+            result = minHeap.poll();
+            
+            //by 2
+            long m = result * 2;
+            if (set.add(m)) {
+                minHeap.offer(m);
+            }
+            //by 3
+            m = result * 3;
+            if (set.add(m)) {
+                minHeap.offer(m);
+            }
+            //by 5
+            m = result * 5;
+            if (set.add(m)) {
+                minHeap.offer(m);
+            }
+        }
+        return (int)result;
+    }
