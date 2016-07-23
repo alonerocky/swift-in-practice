0ddec351e2d3c764e3b3c686f0e213f831458440
alonerocky <lishoulong@gmail.com>
2016-07-23 16:41:49 -0700

    July 23

--- Leetcode/NumArray.java
+++ Leetcode/NumArray.java
@@ -17,3 +17,56 @@ public class NumArray {
         }
     }
 }
+
+public class NumArray {
+    private FenwickTree tree;
+    public NumArray(int[] nums) {
+       tree = new FenwickTree(nums);
+    }
+
+    public int sumRange(int i, int j) {
+        return tree.sum(j+1) - tree.sum(i);
+    }
+    
+    
+}
+class FenwickTree {
+    private int[] nums;
+    private int[] sums;
+    private int n;
+    public FenwickTree(int[] nums) {
+        if (nums != null && nums.length > 0) {
+            this.nums = nums;
+            this.n = nums.length;
+            this.sums = new int[n+1];
+            for(int i = 0; i < n; i++) {
+                add(i+1, nums[i]);
+            }
+        }
+    }
+    
+    public int lowBit(int x) {
+        return x & -x;
+    }
+    
+    public void add(int i, int value) {
+        while(i <= n) {
+            sums[i] += value;
+            i += lowBit(i);
+        }
+    }
+    
+    public int sum(int i) {
+        int result = 0;
+        while(i > 0) {
+            result += sums[i];
+            i -= lowBit(i);
+        }
+        return result;
+    }
+    
+    public void update(int i, int value) {
+         add(i+1, value - nums[i]);
+         nums[i] = value;
+    }
+}
--- Leetcode/NumMatrix.java
+++ Leetcode/NumMatrix.java
@@ -1,4 +1,74 @@
 public class NumMatrix {
+    private FenwickTree2D tree ;
+    public NumMatrix(int[][] matrix) {
+        tree = new FenwickTree2D(matrix);
+    }
+
+    public void update(int row, int col, int val) {
+        tree.update(row, col, val);
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return tree.sumRegion(row1, col1, row2, col2);
+    }
+}
+
+class FenwickTree2D {
+    private int[][] matrix;
+    private int[][] tree;
+    private int m;
+    private int n;
+    public FenwickTree2D(int[][] matrix) {
+        if (matrix != null && matrix.length > 0 && matrix[0].length > 0) {
+            this.matrix = matrix;
+            this.m = matrix.length ;
+            this.n = matrix[0].length ;
+            this.tree = new int[m+1][n+1];
+            for(int i = 0; i < m; i++) {
+                for(int j = 0; j < n; j++) {
+                    add(i, j, matrix[i][j]);
+                }
+            }
+        }
+    }
+
+    public int lowBit(int x) {
+        return x & -x;
+    }
+    //add value to matrix[row][column] , need update the tree
+    public void add(int row, int column, int value) {
+        for(int i = row +1; i <= m; i += lowBit(i)) {
+            for(int j = column + 1; j <= n; j += lowBit(j)) {
+                tree[i][j] += value;
+            }
+        }
+    }
+
+    public void update(int row, int column, int value) {
+        if (matrix[row][column] == value) {
+            return ;
+        }
+        add(row, column, value - matrix[row][column]);
+        matrix[row][column] = value;
+    }
+
+    public int sum(int row, int column) {
+        int result = 0;
+        for(int i = row ; i > 0 ; i -= lowBit(i)) {
+            for(int j = column ; j > 0 ; j -= lowBit(j)) {
+                result += tree[i][j];
+            }
+        }
+        return result;
+    }
+
+    public int sumRegion(int r1, int c1, int r2, int c2) {
+        return sum(r2+1, c2+1) - sum(r2+1, c1) - sum(r1, c2+1) + sum(r1,c1);
+    }
+}
+
+
+public class NumMatrix {
     private int[][] sum;
     private int[][] matrix;
     public NumMatrix(int[][] matrix) {
