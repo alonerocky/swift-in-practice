497673fa9064539832f9772a9498b78f5a7fa9a8
alonerocky <lishoulong@gmail.com>
2016-07-27 18:58:32 -0700

    297. Serialize and Deserialize Binary Tree

--- Leetcode/TreeCodec.java
+++ Leetcode/TreeCodec.java
@@ -87,3 +87,49 @@ public static String serialize(TreeNode root) {
         }
         return root;
     }
+
+
+public String serialize(TreeNode root) {
+        // write your code here
+        if (root == null) {
+            return "";
+        }
+        StringBuffer sb = new StringBuffer();
+        serialize(root, sb);
+        return sb.toString();
+    }
+    public void serialize(TreeNode root, StringBuffer sb) {
+        if (root == null) {
+            sb.append("#,");
+            return;
+        } 
+    }
+    
+    /**
+     * This method will be invoked second, the argument data is what exactly
+     * you serialized at method "serialize", that means the data is not given by
+     * system, it's given by your own serialize method. So the format of data is
+     * designed by yourself, and deserialize it here as you serialize it in 
+     * "serialize" method.
+     */
+    public TreeNode deserialize(String data) {
+        // write your code here
+        if (data == null || data.length() == 0) {
+            return null;
+        }
+        StringTokenizer st = new StringTokenizer(data, ",");
+        return deserialize(st);
+    }
+    public TreeNode deserialize(StringTokenizer st) {
+        if (!st.hasMoreTokens()) {
+            return null;
+        }
+        String val = st.nextToken();
+        if (val.equals("#")) {
+            return null;
+        }
+        TreeNode root = new TreeNode(Integer.parseInt(val));
+        root.left = deserialize(st);
+        root.right = deserialize(st);
+        return root;
+    }
